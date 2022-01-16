9b86d89fd394f552718f4e21db214315437adca6
Alex <aleksa.radisav1994@gmail.com>
2022-01-16 22:01:15 +0100

    divs created, basic sketchpad function created, random color generator created

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.getElementById('container');
+
+//creating 32 divs, appending them to main div 'container'
+const divCounter = Math.pow(16,2);
+for (let i = 0; i < divCounter; i++){
+    const div = document.createElement('div');
+    div.classList.add('square');
+    container.appendChild(div);
+}
+
+const grid = document.querySelectorAll('.square');
+
+//random color generator: 
+// const color = '#' + (Math.random().toString(16) + "000000").substring(2, 8);
+
+grid.forEach((square => {
+    square.addEventListener('mouseenter', () => {
+        const color = '#' + (Math.random().toString(16) + "000000").substring(2, 8);
+        square.style.background = color;
+    });
+}))
+
+
+
